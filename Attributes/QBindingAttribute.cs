5cad369b763d1b740c68389d304577fd691ebf8f
hyunq21 <hyunq60@gmail.com>
2023-05-19 21:32:13 +0900

    test

--- Attributes/QBindingAttribute.cs
+++ Attributes/QBindingAttribute.cs
@@ -1,29 +1,29 @@
 ﻿namespace System
 {
     [System.AttributeUsage(System.AttributeTargets.Class)]
-    public class QBindingAttribute : System.Attribute
+    public sealed class QBindingAttribute : System.Attribute
     {
     }
 
     [System.AttributeUsage(System.AttributeTargets.Field)]
-    public class QNoBindingAttribute : System.Attribute
+    public sealed class QNoBindingAttribute : System.Attribute
     {
     }
 
     [System.AttributeUsage(System.AttributeTargets.Field)]
-    public class QVirtualAttribute : System.Attribute
+    public sealed class QVirtualAttribute : System.Attribute
     {
     }
 
     [System.AttributeUsage(System.AttributeTargets.Field)]
-    public class QEventAttribute : System.Attribute
+    public sealed class QEventAttribute : System.Attribute
     {
         public string PropertyChanging;
         public string PropertyChanged;
     }
 
     [System.AttributeUsage(System.AttributeTargets.Field)]
-    public class QJsonNameAttribute : System.Attribute
+    public sealed class QJsonNameAttribute : System.Attribute
     {
         private string jsonName;
         public QJsonNameAttribute(string name)
@@ -33,7 +33,7 @@
     }
 
     [System.AttributeUsage(System.AttributeTargets.Method)]
-    public class QCommandAttribute : System.Attribute
+    public sealed class QCommandAttribute : System.Attribute
     {
     }
 }
