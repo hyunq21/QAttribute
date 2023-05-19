768f443fc8f42db4c0db123bf02d946884e34352
hyunq21 <hyunq60@gmail.com>
2023-05-19 21:49:44 +0900

    bugfixed

--- Attributes/QBindingAttribute.cs
+++ Attributes/QBindingAttribute.cs
@@ -1,29 +1,29 @@
 ﻿namespace System
 {
     [System.AttributeUsage(System.AttributeTargets.Class)]
-    public sealed class QBindingAttribute : System.Attribute
+    public class QBindingAttribute : System.Attribute
     {
     }
 
     [System.AttributeUsage(System.AttributeTargets.Field)]
-    public sealed class QNoBindingAttribute : System.Attribute
+    public class QNoBindingAttribute : System.Attribute
     {
     }
 
     [System.AttributeUsage(System.AttributeTargets.Field)]
-    public sealed class QVirtualAttribute : System.Attribute
+    public class QVirtualAttribute : System.Attribute
     {
     }
 
     [System.AttributeUsage(System.AttributeTargets.Field)]
-    public sealed class QEventAttribute : System.Attribute
+    public class QEventAttribute : System.Attribute
     {
         public string PropertyChanging;
         public string PropertyChanged;
     }
 
     [System.AttributeUsage(System.AttributeTargets.Field)]
-    public sealed class QJsonNameAttribute : System.Attribute
+    public class QJsonNameAttribute : System.Attribute
     {
         private string jsonName;
         public QJsonNameAttribute(string name)
@@ -33,7 +33,7 @@
     }
 
     [System.AttributeUsage(System.AttributeTargets.Method)]
-    public sealed class QCommandAttribute : System.Attribute
+    public class QCommandAttribute : System.Attribute
     {
     }
 }
